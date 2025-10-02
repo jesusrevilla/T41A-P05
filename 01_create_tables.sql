3778a121f1048287f71a4137f784ae6b75f23df7
VictorCastro135 <178920@upslp.edu.mx>
2025-10-01 22:22:39 -0600

    feature/178920

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,14 +1,24 @@
-CREATE TABLE factura (
+CREATE TABLE clientes (
+    codigo_del_cliente VARCHAR(10) PRIMARY KEY,
+    nombre_del_cliente VARCHAR(30) NOT NULL
+);
 
+CREATE TABLE articulo (
+    codigo_del_articulo VARCHAR(5) PRIMARY KEY,
+    nombre_del_articulo VARCHAR(15) NOT NULL,
+    precio_unitario FLOAT NOT NULL
+);
+
+CREATE TABLE factura (
     sucursal VARCHAR(10) NOT NULL,
     numero_de_factura INT NOT NULL,
     fecha_de_la_factura DATE NOT NULL,
-    forma_de_pago_factura VARCHAR(20) NOT NULL,
+    forma_de_pago_factura VARCHAR(2) NOT NULL,
     codigo_del_cliente VARCHAR(10) NOT NULL,
     total_de_la_factura FLOAT NOT NULL,
     
-    PRIMARY KEY (sucursal, numero_factura),
-    FOREIGN KEY (codigo_cliente) REFERENCES clientes(codigo_cliente)
+    PRIMARY KEY (sucursal, numero_de_factura), 
+    FOREIGN KEY (codigo_del_cliente) REFERENCES clientes(codigo_del_cliente)
 );
 
 CREATE TABLE detalle_de_factura(
@@ -19,18 +29,10 @@ CREATE TABLE detalle_de_factura(
     precio_unitario_del_articulo FLOAT NOT NULL,
     subtotal_del_articulo FLOAT NOT NULL,
     
-    PRIMARY KEY (sucursal, numero_factura, codigo_articulo),
-    FOREIGN KEY (sucursal, numero_factura) REFERENCES factura(sucursal, numero_factura),
-    FOREIGN KEY (codigo_articulo) REFERENCES articulos(codigo_articulo)
+    PRIMARY KEY (sucursal, numero_de_factura, codigo_de_articulo),
+    FOREIGN KEY (sucursal, numero_de_factura) REFERENCES factura(sucursal, numero_de_factura),
+    FOREIGN KEY (codigo_de_articulo) REFERENCES articulo(codigo_del_articulo)
 );
 
-CREATE TABLE articulo (
-    codigo_del_articulo VARCHAR(5) PRIMARY KEY,
-    nombre_del_articulo VARCHAR(15) NOT NULL,
-    precio_unitario FLOAT NOT NULL
-);
 
-CREATE TABLE clientes (
-    codigo_del_cliente VARCHAR(10) PRIMARY KEY,
-    nombre_del_cliente VARCHAR(30) NOT NULL
-);
+
