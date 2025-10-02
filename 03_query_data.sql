735efdd8fa454e222e6c407d8851725ddc55c5ec
Jpablo179804 <179804@upslp.edu.mx>
2025-10-01 19:21:00 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,10 @@
-
+SELECT 
+    f.sucursal,
+    f.numero_factura,
+    f.fecha_factura,
+    c.nombre_cliente,
+    f.forma_pago,
+    f.total_factura
+FROM Factura f
+JOIN Clientes c ON f.codigo_cliente = c.codigo_cliente
+ORDER BY f.fecha_factura DESC;
