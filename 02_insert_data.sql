a0d4b37fb4f97cc8ad341c285f53b4cd378f3f12
Juan Pablo Csnv <175329@upslp.edu.mx>
2025-10-01 19:44:52 -0600

    datos-175329

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -5,16 +5,16 @@ INSERT INTO clientes VALUES
 (2, 'María Gómez');
 
 INSERT INTO articulo VALUES
-(101, 'Teclado', 250.00),
-(102, 'Mouse', 150.00),
-(103, 'Monitor', 3500.00);
+('A101', 'Teclado', 250.00),
+('A102', 'Mouse', 150.00),
+('A103', 'Monitor', 3500.00);
 
 INSERT INTO factura VALUES
 ('Sucursal Norte', 1001, '2025-10-01', 'EFEC', 1, 3750.00),
 ('Sucursal Sur', 1002, '2025-10-01', 'TARJ', 2, 4000.00);
 
 INSERT INTO detalle_de_factura VALUES
-('Sucursal Norte', 1001, 101, 2, 250.00, 500.00),
-('Sucursal Norte', 1001, 103, 1, 3500.00, 3500.00),
-('Sucursal Sur', 1002, 102, 5, 150.00, 750.00),
-('Sucursal Sur', 1002, 103, 1, 3500.00, 3500.00);
+('Sucursal Norte', 1001, 'A101', 2, 250.00, 500.00),
+('Sucursal Norte', 1001, 'A103', 1, 3500.00, 3500.00),
+('Sucursal Sur', 1002, 'A102', 5, 150.00, 750.00),
+('Sucursal Sur', 1002, 'A103', 1, 3500.00, 3500.00);
