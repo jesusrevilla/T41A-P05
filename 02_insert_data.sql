59fb60b47cd586a5bdec977ce535ec02ca205dd9
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-10-01 19:40:31 -0600

    feature/182483

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,21 +1,17 @@
---Clientes
 INSERT INTO clientes (nombre_del_cliente) VALUES 
 ('Juan Pérez'),
 ('María López'),
 ('Carlos Sánchez');
 
--- Artículos
-INSERT INTO articulo (nombre_del_articulo, precio_unitario) VALUES 
-('Laptop', 12000.50),
-('Mouse', 250.00);
+INSERT INTO articulo (codigo_del_articulo, nombre_del_articulo, precio_unitario) VALUES 
+('A100', 'Laptop', 12000.50),
+('B200', 'Mouse', 250.00);
 
--- Facturas
 INSERT INTO factura (sucursal, numero_de_factura, codigo_del_cliente, fecha_de_la_factura, forma_de_pago_factura, total_de_la_factura) VALUES
 (1, 1001, 1, '2025-09-01', 'EF', 12250.50),
 (1, 1002, 2, '2025-09-02', 'TC', 12000.50);
 
--- Detalles facturas
 INSERT INTO detalle_de_factura (sucursal, numero_de_factura, codigo_de_articulo, cantidad_del_articulo, precio_unitario_del_articulo, subtotal_del_articulo) VALUES
-(1, 1001, 1, 1, 12000.50, 12000.50),
-(1, 1001, 2, 1, 250.00, 250.00),
-(1, 1002, 1, 1, 12000.50, 12000.50);
+(1, 1001, 'A100', 1, 12000.50, 12000.50),
+(1, 1001, 'B200', 1, 250.00, 250.00),
+(1, 1002, 'A100', 1, 12000.50, 12000.50);
