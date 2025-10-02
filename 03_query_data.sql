68e4e2e96fe0adb67115f3ab5ee0b697f0446a4c
AngelSaucedo-dev <ange1eduard08122003@gmail.com>
2025-10-01 19:40:33 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,4 +1,4 @@
-SELECT 
+SELECT
     f.sucursal,
     f.numero_de_factura,
     f.fecha_de_la_factura,
@@ -15,4 +15,3 @@ JOIN clientes c ON f.codigo_del_cliente = c.codigo_del_cliente
 JOIN detalle_de_factura df ON f.sucursal = df.sucursal AND f.numero_de_factura = df.numero_de_factura
 JOIN articulo a ON df.codigo_de_articulo = a.codigo_del_articulo
 WHERE f.sucursal = 'S1' AND f.numero_de_factura = 1;
-
