b5dbc35465e23a0b0d4e57f15b53d6599b2823a0
Jpablo179804 <179804@upslp.edu.mx>
2025-10-01 19:36:19 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,33 +1,33 @@
 CREATE TABLE Clientes (
-    codigo_cliente CHAR(3) PRIMARY KEY,
+    codigo_del_cliente CHAR(3) PRIMARY KEY,
     nombre_cliente VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE Articulo (
-    codigo_articulo CHAR(2) PRIMARY KEY,
+    codigo_del_articulo CHAR(2) PRIMARY KEY,
     nombre_articulo VARCHAR(100) NOT NULL,
     precio_unitario DECIMAL(10,2) NOT NULL
 );
 
 CREATE TABLE Factura (
     sucursal CHAR(2) NOT NULL,
-    numero_factura CHAR(3) NOT NULL,
+    numero_de_factura CHAR(3) NOT NULL,
     fecha_factura DATE NOT NULL,
     forma_pago CHAR(2) NOT NULL,
-    codigo_cliente CHAR(3) NOT NULL,
+    codigo_del_cliente CHAR(3) NOT NULL,
     total_factura DECIMAL(10,2) NOT NULL,
-    PRIMARY KEY (sucursal, numero_factura),
-    FOREIGN KEY (codigo_cliente) REFERENCES Clientes(codigo_cliente)
+    PRIMARY KEY (sucursal, numero_de_factura),
+    FOREIGN KEY (codigo_del_cliente) REFERENCES Clientes(codigo_del_cliente)
 );
 
 CREATE TABLE Detalle_Factura (
     sucursal CHAR(2) NOT NULL,
-    numero_factura CHAR(3) NOT NULL,
-    codigo_articulo CHAR(2) NOT NULL,
+    numero_de_factura CHAR(3) NOT NULL,
+    codigo_del_articulo CHAR(2) NOT NULL,
     cantidad INTEGER NOT NULL,
     precio_unitario DECIMAL(10,2) NOT NULL,
     subtotal_articulo DECIMAL(10,2) NOT NULL,
-    PRIMARY KEY (sucursal, numero_factura, codigo_articulo),
-    FOREIGN KEY (sucursal, numero_factura) REFERENCES Factura(sucursal, numero_factura),
-    FOREIGN KEY (codigo_articulo) REFERENCES Articulo(codigo_articulo)
+    PRIMARY KEY (sucursal, numero_de_factura, codigo_del_articulo),
+    FOREIGN KEY (sucursal, numero_de_factura) REFERENCES Factura(sucursal, numero_de_factura),
+    FOREIGN KEY (codigo_del_articulo) REFERENCES Articulo(codigo_del_articulo)
 );
