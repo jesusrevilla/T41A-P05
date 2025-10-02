6635e26cb925c530166f60d8b0bf67b008ae06af
daf01-web <171513@upslp.edu.mx>
2025-10-01 22:55:54 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -4,25 +4,25 @@
        Datos de prueba (los mismos del ejemplo del libro)
    ========================================================= */
 
-/* CLIENTES */
-INSERT INTO "Cliente" ("CodigoCliente", "NombreCliente") VALUES
-('01',  'ALVAREZ'),
+-- CLIENTES
+INSERT INTO clientes (codigo_del_cliente, nombre_del_cliente) VALUES
+('01', 'ALVAREZ'),
 ('107', 'CASTRO');
 
-/* ARTICULOS */
-INSERT INTO "Articulo" ("CodigoArticulo", "NombreArticulo", "PrecioUnitario") VALUES
-('01', 'LÁPIZ', 1.25),
+-- ARTICULOS
+INSERT INTO articulo (codigo_del_articulo, nombre_del_articulo, precio_unitario) VALUES
+('01', 'LAPIZ', 1.25),
 ('02', 'GOMA', 0.75),
-('08', 'COMPÁS', 4.00);
+('08', 'COMPAS', 4.00);
 
-/* FACTURAS */
-INSERT INTO "Factura" ("Sucursal","NumeroFactura","FechaFactura","FormaPagoFactura","CodigoCliente","TotalFactura") VALUES
+-- FACTURAS
+INSERT INTO factura (sucursal, numero_de_factura, fecha_de_la_factura, forma_de_pago_factura, codigo_del_cliente, total_de_la_factura) VALUES
 ('01-500', '1/01/06', '2006-01-01', 'E',  '01', 48.20),
 ('01-501', '2/01/06', '2006-01-02', 'CC', '107', 16.00);
 
-/* DETALLES DE FACTURA */
-INSERT INTO "Detalle_Factura" 
-("Sucursal","NumeroFactura","CodigoArticulo","CantidadArticulo","PrecioUnitarioArticulo","SubtotalArticulo") VALUES
-('01-500','1/01/06','01',3,1.25,3.75),   -- 3 Lápices
-('01-500','1/01/06','02',6,0.75,4.50),   -- 6 Gomas
-('01-501','2/01/06','08',4,4.00,16.00);  -- 4 Compases
+-- DETALLE DE FACTURA
+INSERT INTO detalle_de_factura (sucursal, numero_de_factura, codigo_de_articulo, cantidad_del_articulo, precio_unitario_del_articulo, subtotal_del_articulo) VALUES
+('01-500', '1/01/06', '01', 3, 1.25, 3.75),   -- 3 lápices
+('01-500', '1/01/06', '02', 6, 0.75, 4.50),   -- 6 gomas
+('01-501', '2/01/06', '08', 4, 4.00, 16.00);  -- 4 compases
+
