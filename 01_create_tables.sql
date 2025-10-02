fcbf7cad3b4cff02bbd5c9d683cda933bd27c4e4
Diego0086 <178218@upslp.edu.mx>
2025-10-01 19:38:46 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,34 @@
+CREATE TABLE clientes (
+    codigo_del_cliente int PRIMARY KEY,
+    nombre_del_cliente varchar(50)
+);
+
+CREATE TABLE articulo (
+    codigo_de_articulo int PRIMARY KEY,
+    nombre_del_articulo varchar(50),
+    precio_unitario float 
+);
+
+CREATE TABLE factura (
+    sucursal int,
+    numero_de_factura int,
+    fecha_de_la_factura date,
+    forma_de_pago_factura varchar(50),
+    codigo_del_cliente int,
+    total_de_la_factura float,
+    PRIMARY KEY (sucursal, numero_de_factura),
+    FOREIGN KEY (codigo_del_cliente) REFERENCES clientes(codigo_del_cliente)
+);
+
+CREATE TABLE detalle_de_factura (
+    sucursal int,
+    numero_de_factura int,
+    codigo_de_articulo int,
+    cantidad_del_articulo int,
+    precio_unitario_del_articulo int,
+    subtotal_del_articulo float,
+    PRIMARY KEY (sucursal, numero_de_factura, codigo_de_articulo),
+    FOREIGN KEY (sucursal, numero_de_factura) REFERENCES factura(sucursal, numero_de_factura),
+    FOREIGN KEY (codigo_de_articulo) REFERENCES articulo(codigo_de_articulo)
+);
 
