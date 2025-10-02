49a6c7b01d902e0fd6ae8d4ea7afa0dc879773d9
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-10-01 19:41:20 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,12 @@
+SELECT 
+    c.nom_cliente,
+    a.nom_articulo,
+    df.cantidad
+FROM detalle_factura df
+JOIN factura f 
+    ON df.num_fac = f.num_fac AND df.sucursal = f.sucursal
+JOIN cliente c 
+    ON f.cod_cliente = c.cod_cliente
+JOIN articulo a 
+    ON df.cod_articulo = a.cod_articulo;
 
