a668910b41170db7a546f5c5aa24468f27b340bb
EmmanuelC-137 <160262587+EmmanuelC-137@users.noreply.github.com>
2025-10-01 19:39:20 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,12 @@
-
+SELECT 
+    f.sucursal,
+    f.numero_de_factura,
+    f.total_de_la_factura as total_factura,
+    SUM(df.subtotal_del_articulo) as suma_subtotales,
+    CASE 
+        WHEN f.total_de_la_factura = SUM(df.subtotal_del_articulo) THEN 'CORRECTO'
+        ELSE 'ERROR'
+    END as estado
+FROM factura f
+JOIN detalle_de_factura df ON f.sucursal = df.sucursal AND f.numero_de_factura = df.numero_de_factura
+GROUP BY f.sucursal, f.numero_de_factura, f.total_de_la_factura;
