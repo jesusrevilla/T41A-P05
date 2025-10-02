1009297b7e83bc84e9c18670d1708aaa4dbaa6b3
Yovana-Palencia <174197@upslp.edu.mx>
2025-10-01 19:41:40 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,14 +1,3 @@
-CREATE TABLE facturas (
-    sucursal VARCHAR(10),
-    numero_factura VARCHAR(10),
-    fecha_factura DATE NOT NULL,
-    forma_pago_factura CHAR(1) NOT NULL,
-    codigo_cliente VARCHAR(10) NOT NULL,
-    total_factura NUMERIC(10,2) NOT NULL,
-    PRIMARY KEY (sucursal, numero_factura),
-    FOREIGN KEY (codigo_cliente) REFERENCES clientes(codigo_cliente)
-);
-
 CREATE TABLE clientes (
     codigo_cliente VARCHAR(10) PRIMARY KEY,
     nombre_cliente VARCHAR(100) NOT NULL
@@ -18,6 +7,17 @@ CREATE TABLE articulos (
     codigo_articulo VARCHAR(10) PRIMARY KEY,
     nombre_articulo VARCHAR(100) NOT NULL,
     precio_unitario_articulo NUMERIC(10,2) NOT NULL
+);
+
+CREATE TABLE facturas (
+    sucursal VARCHAR(10),
+    numero_factura VARCHAR(10),
+    fecha_factura DATE NOT NULL,
+    forma_pago_factura CHAR(1) NOT NULL,
+    codigo_cliente VARCHAR(10) NOT NULL,
+    total_factura NUMERIC(10,2) NOT NULL,
+    PRIMARY KEY (sucursal, numero_factura),
+    FOREIGN KEY (codigo_cliente) REFERENCES clientes(codigo_cliente)
 );
 
 CREATE TABLE detalle_factura (
@@ -30,4 +30,3 @@ CREATE TABLE detalle_factura (
     FOREIGN KEY (sucursal, numero_factura) REFERENCES facturas(sucursal, numero_factura),
     FOREIGN KEY (codigo_articulo) REFERENCES articulos(codigo_articulo)
 );
-
