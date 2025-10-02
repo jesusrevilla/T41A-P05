97685e8827bd46246b166169cf9f7211e1f614b5
Cristphertorres21 <cristophertorresdza@gmail.com>
2025-10-01 19:20:37 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,16 @@
 
+INSERT INTO Factura_3FN (sucursal, num_factura, fecha, forma_pago, cod_cliente)
+VALUES 
+('SUC01', 1001, '2025-10-01', 'E', 1),
+('SUC01', 1002, '2025-10-02', 'T', 2),
+('SUC02', 2001, '2025-10-01', 'E', 3),
+('SUC02', 2002, '2025-10-03', 'C', 4),
+('SUC03', 3001, '2025-10-04', 'T', 5);
+
+INSERT INTO DetalleFactura_3FN (sucursal, num_factura, cod_articulo, cantidad)
+VALUES 
+('SUC01', 1001, 101, 2),
+('SUC01', 1001, 102, 1),
+('SUC01', 1002, 103, 3),
+('SUC02', 2001, 101, 1),
+('SUC02', 2002, 104, 5);
