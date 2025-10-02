65bd6d52ac79a59a0e48b22e883b46ddc74a2798
Karina123Kma <upslpbd@gmail.com>
2025-10-01 19:24:14 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,14 @@
+SELECT f.id_factura, f.fecha, f.forma_pago, f.total,
+       s.nombre_sucursal,
+       c.nombre_cliente
+FROM Factura f
+JOIN Sucursal s ON f.id_sucursal = s.id_sucursal
+JOIN Cliente c ON f.id_cliente = c.id_cliente;
+
+SELECT df.id_detalle, df.cantidad, df.subtotal,
+       a.nombre_articulo, a.precio_unitario,
+       f.id_factura, f.fecha, f.forma_pago
+FROM DetalleFactura df
+JOIN Articulo a ON df.id_articulo = a.id_articulo
+JOIN Factura f ON df.id_factura = f.id_factura;
 
