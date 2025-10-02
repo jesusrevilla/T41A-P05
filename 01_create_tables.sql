58f42276bc3eb9c21dd9b1994ba9b7de9bab874d
Diego0086 <178218@upslp.edu.mx>
2025-10-01 23:36:22 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,34 +1,34 @@
 CREATE TABLE clientes (
-    codigo_del_cliente int PRIMARY KEY,
-    nombre_del_cliente varchar(50)
+    codigo_del_cliente VARCHAR(50) PRIMARY KEY,
+    nombre_del_cliente VARCHAR(50)
 );
 
 CREATE TABLE articulo (
-    codigo_del_articulo int PRIMARY KEY,  
-    nombre_del_articulo varchar(50),
-    precio_unitario float
+    codigo_del_articulo VARCHAR(50) PRIMARY KEY,
+    nombre_del_articulo VARCHAR(50),
+    precio_unitario FLOAT
 );
 
 CREATE TABLE factura (
-    sucursal int,
-    numero_de_factura int,
-    fecha_de_la_factura date,
-    forma_de_pago_factura varchar(50),
-    codigo_del_cliente int,
-    total_de_la_factura float,
+    sucursal VARCHAR(50),
+    numero_de_factura INT,
+    fecha_de_la_factura DATE,
+    forma_de_pago_factura VARCHAR(50),
+    codigo_del_cliente VARCHAR(50),
+    total_de_la_factura FLOAT,
     PRIMARY KEY (sucursal, numero_de_factura),
     FOREIGN KEY (codigo_del_cliente) REFERENCES clientes(codigo_del_cliente)
 );
 
 CREATE TABLE detalle_de_factura (
-    sucursal int,
-    numero_de_factura int,
-    codigo_del_articulo int,              
-    cantidad_del_articulo int,
-    precio_unitario_del_articulo int,
-    subtotal_del_articulo float,
-    PRIMARY KEY (sucursal, numero_de_factura, codigo_del_articulo),
+    sucursal VARCHAR(50),
+    numero_de_factura INT,
+    codigo_de_articulo VARCHAR(50),
+    cantidad_del_articulo INT,
+    precio_unitario_del_articulo FLOAT, 
+    subtotal_del_articulo FLOAT,
+    PRIMARY KEY (sucursal, numero_de_factura, codigo_de_articulo),
     FOREIGN KEY (sucursal, numero_de_factura) REFERENCES factura(sucursal, numero_de_factura),
-    FOREIGN KEY (codigo_del_articulo) REFERENCES articulo(codigo_del_articulo)
+    FOREIGN KEY (codigo_de_articulo) REFERENCES articulo(codigo_del_articulo)
 );
 
