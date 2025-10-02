96d56e9d44bb9615679473afe97ce5f43cc3232b
m00n07 <174653@upslp.edu.mx>
2025-10-01 20:49:57 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,13 +1,5 @@
-SELECT 
-  f.sucursal,
-  f.numero_de_factura,
-  f.fecha_de_la_factura,
-  c.nombre_del_cliente,
-  a.nombre_de_articulo,
-  df.cantidad_del_articulo,
-  df.precio_unitario_del_articulo,
-  df.subtotal_del_articulo
+SELECT f.sucursal, f.numero_de_factura, f.fecha_de_la_factura,
+       c.nombre_del_cliente, f.total_de_la_factura
 FROM factura f
-JOIN clientes c ON f.codigo_del_cliente = c.codigo_del_cliente
-JOIN detalle_de_factura df ON f.sucursal = df.sucursal AND f.numero_de_factura = df.numero_de_factura
-JOIN articulo a ON df.codigo_de_articulo = a.codigo_del_articulo;
+JOIN clientes c ON c.codigo_del_cliente = f.codigo_del_cliente
+ORDER BY f.sucursal, f.numero_de_factura;
