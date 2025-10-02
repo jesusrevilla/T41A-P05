409564900be73a14ac33a18177f979e2fd93121c
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-10-01 19:31:29 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,16 @@
+INSERT INTO cliente values
+(1,'Bryan Gamez'),
+(2,'Coral Dominguez');
 
+INSERT INTO articulo values
+(1,'Lapiz',5.0),
+(2,'Goma',4.0);
+
+INSERT INTO factura values
+(1,2,'2025-10-01','E',1,10.0),
+(1,3,'2025-10-01','E',2,9.0);
+
+INSERT INTO detalle_factura values
+(1,2,1,2,5.0,10.0),
+(1,3,1,1,5.0,5.0),
+(1,3,2,1,4.0,4.0);
