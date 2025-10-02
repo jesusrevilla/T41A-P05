c706ca667e0b8dc87cfc35cd7274aa3dbaccdaf1
aldogg10 <179862@upslp.edu.mx>
2025-10-01 19:43:35 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,4 +1,4 @@
 INSERT INTO cliente VALUES ('01', 'Cristiano Ronaldo');
-INSERT INTO articulo VALUES ('01', 'Television', '8000.00');
+INSERT INTO articulo VALUES ('A100', 'Television', '8000.00');
 INSERT INTO factura VALUES ('001', '1', '2025-08-13', 'E', '01');
-INSERT INTO detalle_factura VALUES ('001', '1', '01', '2');
+INSERT INTO detalle_factura VALUES ('001', '1', 'A100', '2');
