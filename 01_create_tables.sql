1f30e1e6e26afdd12e797b6f33aa642c8b9cc63b
menaxixi <179800@upslp.edu.mx>
2025-10-01 19:31:13 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,20 +1,19 @@
--- en el modelo 3FN
-CREATE TABLE sucursal3 (
+CREATE TABLE sucursal3(
   id_sucursal3 SERIAL PRIMARY KEY,
   nombre_sucursal3 VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE forma_pago3 (
+CREATE TABLE forma_pago3(
   id_forma_pago3 SERIAL PRIMARY KEY,
   tipo_pago3 VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE cliente3 (
+CREATE TABLE cliente3(
   codigo_cliente3 SERIAL PRIMARY KEY,
   nombre_cliente3 VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE factura3 (
+CREATE TABLE factura3(
   id_factura3 SERIAL PRIMARY KEY,
   codigo_cliente3 INT NOT NULL,
   id_sucursal3 INT NOT NULL,
@@ -26,13 +25,13 @@ CREATE TABLE factura3 (
   FOREIGN KEY (id_forma_pago3) REFERENCES forma_pago3(id_forma_pago3)
 );
 
-CREATE TABLE articulo3 (
+CREATE TABLE articulo3(
   codigo_articulo3 SERIAL PRIMARY KEY,
   nombre_articulo3 VARCHAR(100) NOT NULL,
   precio_unitario3 NUMERIC(10, 2) NOT NULL
 );
 
-CREATE TABLE detalles_factura3 (
+CREATE TABLE detalles_factura3(
   id_factura3 INT NOT NULL,
   codigo_articulo3 INT NOT NULL,
   cantidad_articulo3 INT NOT NULL,
