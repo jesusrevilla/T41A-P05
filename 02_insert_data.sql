a90e75b4e21c2c2fb81c2da290fa048e5ecc1950
EmmanuelC-137 <160262587+EmmanuelC-137@users.noreply.github.com>
2025-10-01 19:44:55 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,36 +1,24 @@
--- Insertar datos en la tabla Sucursal
-INSERT INTO Sucursal (sucursal, nombre_sucursal) VALUES
-('01', 'Sucursal 01'),
-('02', 'Sucursal 02');
-
--- Insertar datos en la tabla clientes
 INSERT INTO clientes (codigo_del_cliente, nombre_del_cliente) VALUES
-('01', 'ALVAREZ'),
-('107', 'CASTRO'),
-('110', 'LIZ');
+(100, 'Cliente Prueba');
+('C001', 'Ana S.A. de C.V.'),
+('C002', 'Beta, S. de R.L.');
 
--- Insertar datos en la tabla articulo
 INSERT INTO articulo (codigo_del_articulo, nombre_del_articulo, precio_unitario) VALUES
-('01', 'LÁPIZ', 1.25),
-('02', 'GOMA', 0.75),
-('08', 'COMPÁS', 4.00),
-('10', 'HOJAS', 5.00),
-('20', 'REGLA', 2.45);
+('A100', 'Articulo Prueba', 50.00);
+('A001', 'Arroz 1kg', 25.00),
+('A002', 'Frijol 1kg', 30.00),
+('A003', 'Aceite 1L', 50.00);
 
--- Insertar datos en la tabla factura
-INSERT INTO factura (sucursal, numero_de_factura, codigo_del_cliente, fecha_de_la_factura, forma_de_pago_factura, total_de_la_factura) VALUES
-('01', '500', '01', '2006-01-01', 'E', 48.20),
-('01', '501', '107', '2006-01-02', 'CC', 16.00),
-('02', '500', '110', '2006-01-03', 'E', 14.90);
+INSERT INTO factura (sucursal, numero_de_factura, fecha_de_la_factura, forma_de_pago_factura, codigo_del_cliente, total_de_la_factura) VALUES
+('S1', 1, CURRENT_DATE, 'Efectivo', 100, 100.00);
+('S1', 1, CURRENT_DATE, 'Efectivo', 'C001', 80.00),
+('S1', 2, CURRENT_DATE, 'Tarjeta',  'C002', 50.00);
 
--- Insertar datos en la tabla detalle_de_factura
 INSERT INTO detalle_de_factura (sucursal, numero_de_factura, codigo_de_articulo, cantidad_del_articulo, precio_unitario_del_articulo, subtotal_del_articulo) VALUES
--- Factura 01-500
-('01', '500', '01', 3, 1.25, 3.75),
-('01', '500', '02', 6, 0.75, 4.50),
-('01', '500', '10', 8, 5.00, 40.00),
--- Factura 01-501
-('01', '501', '08', 4, 4.00, 16.00),
--- Factura 02-500
-('02', '500', '20', 2, 2.45, 4.90),
-('02', '500', '10', 2, 5.00, 10.00);
+('S1', 1, 'A100', 2, 50.00, 100.00);
+INSERT INTO detalle_de_factura
+(sucursal, numero_de_factura, codigo_de_articulo, cantidad_del_articulo, precio_unitario_del_articulo, subtotal_del_articulo) VALUES
+('S1', 1, 'A001', 2, 25.00, 50.00),
+('S1', 1, 'A002', 1, 30.00, 30.00),
+
+('S1', 2, 'A003', 1, 50.00, 50.00);
