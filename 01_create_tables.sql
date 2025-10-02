4316d1046e49ab085578ab91bf32018191b707bd
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-10-01 19:32:08 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,35 +1,39 @@
 --Tablas en 3FN
-CREATE TABLE cliente(
-    codigo_cliente SERIAL PRIMARY KEY,
-    nombre_cliente VARCHAR(50) NOT NULL
+CREATE TABLE clientes(
+    codigo_del_cliente SERIAL PRIMARY KEY,
+    nombre_del_cliente VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE factura(
     sucursal INT NOT NULL,
-    numero_factura INT NOT NULL,
-    codigo_cliente INT NOT NULL,
-    fecha_factura DATE NOT NULL,
-    forma_pago CHAR(2) NOT NULL,
-    total_factura NUMERIC(10,2) NOT NULL,
-    PRIMARY KEY (sucursal, numero_factura),
-    FOREIGN KEY (codigo_cliente)
-        REFERENCES cliente(codigo_cliente)
+    numero_de_factura INT NOT NULL,
+    codigo_del_cliente INT NOT NULL,
+    fecha_de_la_factura DATE NOT NULL,
+    forma_de_pago_factura CHAR(2) NOT NULL,
+    total_de_la_factura NUMERIC(10,2) NOT NULL,
+    PRIMARY KEY (sucursal, numero_de_factura),
+    FOREIGN KEY (codigo_del_cliente)
+        REFERENCES clientes(codigo_del_cliente)
 );
 
 CREATE TABLE articulo(
-    codigo_articulo SERIAL PRIMARY KEY,
-    nombre_articulo VARCHAR(50) NOT NULL,
+    codigo_del_articulo SERIAL PRIMARY KEY,
+    nombre_del_articulo VARCHAR(50) NOT NULL,
     precio_unitario NUMERIC(10,2) NOT NULL
 );
 
-CREATE TABLE detalle_factura(
+CREATE TABLE detalle_de_factura(
     sucursal INT NOT NULL,
-    numero_factura INT NOT NULL,
-    codigo_articulo INT NOT NULL,
-    cantidad_articulo INT NOT NULL,
-    subtotal_articulo NUMERIC(10,2) NOT NULL,
-    PRIMARY KEY (sucursal, numero_factura, codigo_articulo),
-    FOREIGN KEY (codigo_articulo)
-        REFERENCES articulo(codigo_articulo)
+    numero_de_factura INT NOT NULL,
+    codigo_de_articulo INT NOT NULL,
+    cantidad_del_articulo INT NOT NULL,
+    precio_unitario_del_articulo NUMERIC(10,2) NOT NULL,
+    subtotal_del_articulo NUMERIC(10,2) NOT NULL,
+    PRIMARY KEY (sucursal, numero_de_factura, codigo_de_articulo),
+    FOREIGN KEY (codigo_de_articulo)
+        REFERENCES articulo(codigo_del_articulo),
+    FOREIGN KEY (sucursal, numero_de_factura)
+        REFERENCES factura(sucursal, numero_de_factura)
 );
+
 
