f6cdfbadbaa51c72032a084044b2a2db99e84fed
daf01-web <171513@upslp.edu.mx>
2025-10-01 23:30:16 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -4,25 +4,27 @@
 -- Tablas: clientes, articulo, factura, detalle_de_factura
 -- ========================================================
 
+-- 01_create_tables.sql
+
 -- CLIENTES
-CREATE TABLE clientes (
+CREATE TABLE IF NOT EXISTS clientes (
   codigo_del_cliente   VARCHAR(10) PRIMARY KEY,
   nombre_del_cliente   VARCHAR(100) NOT NULL
 );
 
 -- ARTICULO
-CREATE TABLE articulo (
+CREATE TABLE IF NOT EXISTS articulo (
   codigo_del_articulo       VARCHAR(10) PRIMARY KEY,
   nombre_del_articulo       VARCHAR(100) NOT NULL,
   precio_unitario           NUMERIC(10,2) NOT NULL CHECK (precio_unitario >= 0)
 );
 
 -- FACTURA
-CREATE TABLE factura (
+CREATE TABLE IF NOT EXISTS factura (
   sucursal                  VARCHAR(10)  NOT NULL,
-  numero_de_factura         VARCHAR(20)  NOT NULL,
+  numero_de_factura         INTEGER      NOT NULL,
   fecha_de_la_factura       DATE         NOT NULL,
-  forma_de_pago_factura     VARCHAR(10)  NOT NULL,
+  forma_de_pago_factura     VARCHAR(30)  NOT NULL,
   codigo_del_cliente        VARCHAR(10)  NOT NULL,
   total_de_la_factura       NUMERIC(12,2) NOT NULL CHECK (total_de_la_factura >= 0),
   CONSTRAINT pk_factura PRIMARY KEY (sucursal, numero_de_factura),
@@ -31,9 +33,9 @@ CREATE TABLE factura (
 );
 
 -- DETALLE DE FACTURA
-CREATE TABLE detalle_de_factura (
+CREATE TABLE IF NOT EXISTS detalle_de_factura (
   sucursal                          VARCHAR(10)  NOT NULL,
-  numero_de_factura                 VARCHAR(20)  NOT NULL,
+  numero_de_factura                 INTEGER      NOT NULL,
   codigo_de_articulo                VARCHAR(10)  NOT NULL,
   cantidad_del_articulo             INTEGER      NOT NULL CHECK (cantidad_del_articulo > 0),
   precio_unitario_del_articulo      NUMERIC(10,2) NOT NULL CHECK (precio_unitario_del_articulo >= 0),
@@ -46,3 +48,4 @@ CREATE TABLE detalle_de_factura (
     FOREIGN KEY (codigo_de_articulo)
     REFERENCES articulo(codigo_del_articulo)
 );
+
