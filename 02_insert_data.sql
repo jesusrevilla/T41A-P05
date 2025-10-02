1f979b2d781c8993d8b6c9342466cb40abc04689
patymtz17 <181730@upslp.edu.mx>
2025-10-01 19:19:00 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -4,14 +4,14 @@ INSERT INTO cliente4 (codigo_cliente, nombre_cliente) VALUES
 (3, 'Carlos Sánchez');
 
 INSERT INTO articulo4 (codigo_articulo, nombre_articulo, precio_unitario) VALUES
-(101, 'Laptop Lenovo', 15000.50),
-(102, 'Mouse Inalámbrico', 350.75),
-(103, 'Teclado Mecánico', 1200.00);
+(101, 'Laptop', 15000.50),
+(102, 'Mouse', 350.75),
+(103, 'Teclado', 1200.00);
 
 INSERT INTO factura4 (id_factura, sucursal, fecha, forma_pago) VALUES
-(1001, 'Sucursal Centro', '2025-09-01', 'Tarjeta'),
-(1002, 'Sucursal Norte', '2025-09-02', 'Efectivo'),
-(1003, 'Sucursal Sur', '2025-09-03', 'Transferencia');
+(1001, 'Plaza San Luis', '2025-09-01', 'Tarjeta'),
+(1002, 'Plaza El Dorado', '2025-09-02', 'Efectivo'),
+(1003, 'Plaza The Park', '2025-09-03', 'Transferencia');
 
 INSERT INTO detalles_factura4 (id_detalle, id_factura, codigo_articulo, cantidad) VALUES
 (1, 1001, 101, 1),
