7d427ad61737a6e79aeb4676af984461d927fdd7
EmmanuelC-137 <160262587+EmmanuelC-137@users.noreply.github.com>
2025-10-01 19:36:36 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,37 +1,36 @@
-
 -- Insertar datos en la tabla Sucursal
-INSERT INTO Sucursal (id_sucursal, nombre_sucursal) VALUES
+INSERT INTO Sucursal (sucursal, nombre_sucursal) VALUES
 ('01', 'Sucursal 01'),
 ('02', 'Sucursal 02');
 
--- Insertar datos en la tabla Cliente
-INSERT INTO Cliente (id_cliente, nombre_cliente) VALUES
+-- Insertar datos en la tabla clientes
+INSERT INTO clientes (codigo_del_cliente, nombre_del_cliente) VALUES
 ('01', 'ALVAREZ'),
 ('107', 'CASTRO'),
 ('110', 'LIZ');
 
--- Insertar datos en la tabla Articulo
-INSERT INTO Articulo (id_articulo, nombre_articulo, precio_unitario) VALUES
+-- Insertar datos en la tabla articulo
+INSERT INTO articulo (codigo_del_articulo, nombre_del_articulo, precio_unitario) VALUES
 ('01', 'LÁPIZ', 1.25),
 ('02', 'GOMA', 0.75),
 ('08', 'COMPÁS', 4.00),
 ('10', 'HOJAS', 5.00),
 ('20', 'REGLA', 2.45);
 
--- Insertar datos en la tabla Factura
-INSERT INTO Factura (id_factura, id_sucursal, id_cliente, fecha, forma_pago, total) VALUES
-('01-500', '01', '01', '2006-01-01', 'E', 48.20),
-('01-501', '01', '107', '2006-01-02', 'CC', 16.00),
-('02-500', '02', '110', '2006-01-03', 'E', 14.90);
+-- Insertar datos en la tabla factura
+INSERT INTO factura (sucursal, numero_de_factura, codigo_del_cliente, fecha_de_la_factura, forma_de_pago_factura, total_de_la_factura) VALUES
+('01', '500', '01', '2006-01-01', 'E', 48.20),
+('01', '501', '107', '2006-01-02', 'CC', 16.00),
+('02', '500', '110', '2006-01-03', 'E', 14.90);
 
--- Insertar datos en la tabla DetalleFactura
-INSERT INTO DetalleFactura (id_factura, id_articulo, cantidad, subtotal) VALUES
+-- Insertar datos en la tabla detalle_de_factura
+INSERT INTO detalle_de_factura (sucursal, numero_de_factura, codigo_de_articulo, cantidad_del_articulo, precio_unitario_del_articulo, subtotal_del_articulo) VALUES
 -- Factura 01-500
-('01-500', '01', 3, 3.75),
-('01-500', '02', 6, 4.50),
-('01-500', '10', 8, 40.00),
+('01', '500', '01', 3, 1.25, 3.75),
+('01', '500', '02', 6, 0.75, 4.50),
+('01', '500', '10', 8, 5.00, 40.00),
 -- Factura 01-501
-('01-501', '08', 4, 16.00),
+('01', '501', '08', 4, 4.00, 16.00),
 -- Factura 02-500
-('02-500', '20', 2, 4.90),
-('02-500', '10', 2, 10.00);
+('02', '500', '20', 2, 2.45, 4.90),
+('02', '500', '10', 2, 5.00, 10.00);
