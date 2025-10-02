d385dc16c5127c4fb674904d14d6c7bebca9c541
Cruz <183213@upslp.edu.mx>
2025-10-01 21:54:40 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE clientes(
-  codigo_de_cliente VARCHAR(10) PRIMARY KEY,
+  codigo_del_cliente VARCHAR(10) PRIMARY KEY,
   nombre_del_cliente VARCHAR(50) NOT NULL
 );
 
@@ -9,7 +9,7 @@ CREATE TABLE factura(
   fecha_de_la_factura TIMESTAMP NOT NULL,
   forma_de_pago_factura VARCHAR(12) NOT NULL,
   codigo_de_cliente VARCHAR(10) NOT NULL,--Lo utilizo como foranea
-  FOREIGN KEY(codigo_de_cliente) REFERENCES clientes(codigo_de_cliente),
+  FOREIGN KEY(codigo_de_cliente) REFERENCES clientes(codigo_del_cliente),
   total_de_la_factura NUMERIC(10,2) NOT NULL,
   PRIMARY KEY(sucursal,numero_de_factura)
 );
