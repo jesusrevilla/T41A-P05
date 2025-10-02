46185cccf00cea99761c65341501deca52759420
Fano-Rdz <178584@upslp.edu.mx>
2025-10-01 19:32:14 -0600

    Query

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,5 @@
-
+SELECT F.sucursal, F.numero_factura, F.fecha_factura, C.nombre_cliente, FP.forma_pago, SUM(DF.cantidad_articulo * DF.precio_unitario) AS total_factura
+FROM Factura F
+JOIN Cliente C ON F.codigo_cliente = C.codigo_cliente JOIN FormaPago FP ON F.forma_pago = FP.forma_pago JOIN DetalleFactura DF ON F.sucursal = DF.sucursal AND F.numero_factura = DF.numero_factura
+GROUP BY F.sucursal, F.numero_factura, F.fecha_factura, C.nombre_cliente, FP.forma_pago
+ORDER BY F.fecha_factura;
