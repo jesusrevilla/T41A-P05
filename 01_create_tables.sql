bd276972d822f0a8c33b0bde0e2423287598d155
Jpablo179804 <179804@upslp.edu.mx>
2025-10-01 19:39:47 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,18 +1,18 @@
 CREATE TABLE Clientes (
     codigo_del_cliente CHAR(3) PRIMARY KEY,
-    nombre_cliente VARCHAR(100) NOT NULL
+    nombre_del_cliente VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE Articulo (
     codigo_del_articulo CHAR(2) PRIMARY KEY,
-    nombre_articulo VARCHAR(100) NOT NULL,
+    nombre_del_articulo VARCHAR(100) NOT NULL,
     precio_unitario DECIMAL(10,2) NOT NULL
 );
 
 CREATE TABLE Factura (
     sucursal CHAR(2) NOT NULL,
     numero_de_factura CHAR(3) NOT NULL,
-    fecha_factura DATE NOT NULL,
+    fecha_de_la_factura DATE NOT NULL,
     forma_pago CHAR(2) NOT NULL,
     codigo_del_cliente CHAR(3) NOT NULL,
     total_factura DECIMAL(10,2) NOT NULL,
