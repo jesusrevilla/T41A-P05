acd3c0e105fedd7328c02b50b1641f25906d62d2
Cristphertorres21 <cristophertorresdza@gmail.com>
2025-10-01 19:22:47 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,16 @@
+SELECT * 
+FROM Factura_3FN;
+
+SELECT * 
+FROM DetalleFactura_3FN;
+
+SELECT f.sucursal, f.num_factura, f.fecha, f.forma_pago, f.cod_cliente, c.nombre_cliente
+FROM Factura_3FN f
+JOIN Cliente_2FN c ON f.cod_cliente = c.cod_cliente;
+
+SELECT f.sucursal, f.num_factura, d.cod_articulo, a.nombre_articulo, d.cantidad
+FROM Factura_3FN f
+JOIN DetalleFactura_3FN d ON f.sucursal = d.sucursal AND f.num_factura = d.num_factura
+JOIN Articulo_2FN a ON d.cod_articulo = a.cod_articulo
+ORDER BY f.sucursal, f.num_factura;
 
