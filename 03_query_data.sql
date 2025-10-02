18d855bc8edc46344ab85bb5cb1c9a177023eed2
Cristphertorres21 <cristophertorresdza@gmail.com>
2025-10-01 19:36:24 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,16 +1,15 @@
-SELECT * 
-FROM Factura_3FN;
+SELECT * FROM factura;
 
-SELECT * 
-FROM DetalleFactura_3FN;
+SELECT * FROM detalle_de_factura;
 
-SELECT f.sucursal, f.num_factura, f.fecha, f.forma_pago, f.cod_cliente, c.nombre_cliente
-FROM Factura_3FN f
-JOIN Cliente_2FN c ON f.cod_cliente = c.cod_cliente;
+SELECT f.sucursal, f.numero_de_factura, f.fecha_de_la_factura,
+       f.forma_de_pago_factura, f.total_de_la_factura, c.nombre_del_cliente
+FROM factura f
+JOIN clientes c ON f.codigo_del_cliente = c.codigo_del_cliente;
 
-SELECT f.sucursal, f.num_factura, d.cod_articulo, a.nombre_articulo, d.cantidad
-FROM Factura_3FN f
-JOIN DetalleFactura_3FN d ON f.sucursal = d.sucursal AND f.num_factura = d.num_factura
-JOIN Articulo_2FN a ON d.cod_articulo = a.cod_articulo
-ORDER BY f.sucursal, f.num_factura;
-
+SELECT f.sucursal, f.numero_de_factura, d.codigo_de_articulo, a.nombre_del_articulo,
+       d.cantidad_del_articulo, d.precio_unitario_del_articulo, d.subtotal_del_articulo
+FROM detalle_de_factura d
+JOIN factura f ON d.sucursal = f.sucursal AND d.numero_de_factura = f.numero_de_factura
+JOIN articulo a ON d.codigo_de_articulo = a.codigo_del_articulo
+ORDER BY f.sucursal, f.numero_de_factura;
