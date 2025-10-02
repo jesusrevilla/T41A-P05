6e93953e2ef9381eaa1a43fe2c81dcb89b7c393f
Fano-Rdz <178584@upslp.edu.mx>
2025-10-01 19:51:56 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -4,9 +4,9 @@ INSERT INTO forma_de_pago (forma_pago) VALUES ('TRNS');
 
 
 INSERT INTO clientes (codigo_del_cliente, nombre_del_cliente)
-VALUES (1, 'Juan Pérez'),
-       (2, 'María López'),
-       (3, 'Carlos Gómez');
+VALUES ('1', 'Juan Pérez'),
+       ('2', 'María López'),
+       ('3', 'Carlos Gómez');
 
 INSERT INTO articulo (codigo_del_articulo, nombre_del_articulo, precio_unitario)
 VALUES 
@@ -16,13 +16,13 @@ VALUES
 
 INSERT INTO factura (sucursal, numero_de_factura, fecha_de_la_factura, forma_de_pago_factura, codigo_del_cliente, total_de_la_factura)
 VALUES 
-  ('Sucursal A', 1001, '2025-09-25', 'EFEC', 1, 31.98),
-  ('Sucursal B', 1002, '2025-09-26', 'TCRD', 2, 49.99),
-  ('Sucursal A', 1003, '2025-09-27', 'TRNS', 3, 120.00);
+  ('Sucursal A', 1001, '2025-09-25', 'EFEC', '1', 31.98),
+  ('Sucursal B', 1002, '2025-09-26', 'TCRD', '2', 49.99),
+  ('Sucursal A', 1003, '2025-09-27', 'TRNS', '3', 120.00);
 
 
 INSERT INTO detalle_de_factura (sucursal, numero_de_factura, codigo_de_articulo, cantidad_del_articulo, precio_unitario_del_articulo)
 VALUES 
-  ('Sucursal A', 1001, 101, 2, 15.99), 
-  ('Sucursal B', 1002, 102, 1, 49.99),  
-  ('Sucursal A', 1003, 103, 1, 120.00);  
+  ('Sucursal A', 1001, '101', '2', 15.99), 
+  ('Sucursal B', 1002, '102', '1', 49.99),  
+  ('Sucursal A', 1003, '103', '1', 120.00);  
