7dd96ddb95f78d7778734459e23b5d8164322f78
Cruz <183213@upslp.edu.mx>
2025-10-01 21:58:57 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -6,5 +6,5 @@ INSERT INTO factura(sucursal,numero_de_factura,fecha_de_la_factura,forma_de_pago
 INSERT INTO articulo(codigo_de_articulo,nombre_del_articulo,precio_unitario) VALUES
   ('15','salchicha',20.50),('58','huevo',49.20),('A100', 'Articulo Prueba', 50.00);
 
-INSERT INTO detalleFactura(sucursal,numero_de_factura,codigo_de_articulo,cantidad_del_articulo,precio_unitario_del_articulo,subtotal_del_articulo) VALUES
+INSERT INTO detalle_de_factura(sucursal,numero_de_factura,codigo_de_articulo,cantidad_del_articulo,precio_unitario_del_articulo,subtotal_del_articulo) VALUES
 ('Paseo',198,'15',2,20.50,41.50),('Dorado',193,'58',1,49.20,49.20),('S1', 1, 'A100', 2, 50.00, 100.00);
