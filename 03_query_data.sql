6e9c984a2c5442fa4bd81c4771d9221c615365f0
aldogg10 <179862@upslp.edu.mx>
2025-10-01 19:26:47 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,7 @@
-
+SELECT f.sucursal,
+       f.numero_factura,
+       f.fecha_factura,
+       f.forma_pago,
+       c.nombre_cliente
+FROM factura f
+JOIN cliente c ON f.codigo_cliente = c.codigo_cliente;
