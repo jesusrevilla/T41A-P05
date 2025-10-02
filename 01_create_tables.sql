7e067d1c9775b333667b38f902f29de8ebf8d0eb
patymtz17 <181730@upslp.edu.mx>
2025-10-01 19:15:19 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,26 @@
+CREATE TABLE factura4 (
+  id_factura INT PRIMARY KEY,
+  sucursal TEXT NOT NULL,
+  fecha DATE NOT NULL,
+  forma_pago TEXT NOT NULL
+);
 
+CREATE TABLE cliente4 (
+  codigo_cliente INT PRIMARY KEY,
+  nombre_cliente TEXT NOT NULL
+);
+
+CREATE TABLE articulo4 (
+  codigo_articulo INT PRIMARY KEY,
+  nombre_articulo TEXT NOT NULL,
+  precio_unitario FLOAT NOT NULL
+);
+
+CREATE TABLE detalles_factura4 (
+  id_detalle INT PRIMARY KEY,
+  id_factura INT NOT NULL,
+  codigo_articulo INT NOT NULL,
+  cantidad INT NOT NULL,
+  FOREIGN KEY (id_factura) REFERENCES factura4(id_factura),
+  FOREIGN KEY (codigo_articulo) REFERENCES articulo4(codigo_articulo)
+);
