69bafa92e85d4a7fc1a0c31ede294b76009a7613
AngelSaucedo-dev <ange1eduard08122003@gmail.com>
2025-10-01 19:22:42 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,16 @@
+SELECT 
+    f.sucursal,
+    f.num_factura,
+    f.fecha,
+    f.forma_pago,
+    f.cod_cliente,
+    df.cod_articulo,
+    df.cantidad
+FROM 
+    Factura_3FN f
+JOIN 
+    DetalleFactura_3FN df 
+    ON f.sucursal = df.sucursal AND f.num_factura = df.num_factura
+WHERE 
+    f.num_factura = 1001 AND f.sucursal = 'SUC001';
 
