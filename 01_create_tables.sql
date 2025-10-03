21afc1b37ee965bee4658590f9ebfde10e4077c8
Jpablo179804 <179804@upslp.edu.mx>
2025-10-03 10:43:27 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,33 +1,33 @@
-CREATE TABLE Clientes (
-    codigo_del_cliente CHAR(4) PRIMARY KEY,
+CREATE TABLE clientes (
+    codigo_del_cliente VARCHAR(50) PRIMARY KEY,
     nombre_del_cliente VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE Articulo (
-    codigo_del_articulo CHAR(4) PRIMARY KEY,
+CREATE TABLE articulo (
+    codigo_del_articulo VARCHAR(50) PRIMARY KEY,
     nombre_del_articulo VARCHAR(100) NOT NULL,
     precio_unitario DECIMAL(10,2) NOT NULL
 );
 
-CREATE TABLE Factura (
-    sucursal CHAR(2) NOT NULL,
-    numero_de_factura INTEGER NOT NULL,
+CREATE TABLE factura (
+    sucursal VARCHAR(50),
+    numero_de_factura INTEGER,
     fecha_de_la_factura DATE NOT NULL,
-    forma_de_pago_factura VARCHAR(20) NOT NULL,
-    codigo_del_cliente CHAR(4) NOT NULL,
+    forma_de_pago_factura VARCHAR(50) NOT NULL,
+    codigo_del_cliente VARCHAR(50) NOT NULL,
     total_de_la_factura DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (sucursal, numero_de_factura),
-    FOREIGN KEY (codigo_del_cliente) REFERENCES Clientes(codigo_del_cliente)
+    FOREIGN KEY (codigo_del_cliente) REFERENCES clientes(codigo_del_cliente)
 );
 
-CREATE TABLE Detalle_de_Factura (
-    sucursal CHAR(2) NOT NULL,
-    numero_de_factura INTEGER NOT NULL,
-    codigo_de_articulo CHAR(4) NOT NULL,
+CREATE TABLE detalle_de_factura (
+    sucursal VARCHAR(50),
+    numero_de_factura INTEGER,
+    codigo_de_articulo VARCHAR(50),
     cantidad_del_articulo INTEGER NOT NULL,
     precio_unitario_del_articulo DECIMAL(10,2) NOT NULL,
     subtotal_del_articulo DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (sucursal, numero_de_factura, codigo_de_articulo),
-    FOREIGN KEY (sucursal, numero_de_factura) REFERENCES Factura(sucursal, numero_de_factura),
-    FOREIGN KEY (codigo_de_articulo) REFERENCES Articulo(codigo_del_articulo)
+    FOREIGN KEY (sucursal, numero_de_factura) REFERENCES factura(sucursal, numero_de_factura),
+    FOREIGN KEY (codigo_de_articulo) REFERENCES articulo(codigo_del_articulo)
 );
