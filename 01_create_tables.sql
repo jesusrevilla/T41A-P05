4bed206f8944384bbb29179491d79ba5e2a807e8
Jpablo179804 <179804@upslp.edu.mx>
2025-10-03 10:21:39 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -11,9 +11,9 @@ CREATE TABLE Articulo (
 
 CREATE TABLE Factura (
     sucursal CHAR(2) NOT NULL,
-    numero_de_factura CHAR(3) NOT NULL,
+    numero_de_factura INTEGER NOT NULL, 
     fecha_de_la_factura DATE NOT NULL,
-    forma_de_pago_factura VARCHAR(20) NOT NULL,  
+    forma_de_pago_factura VARCHAR(20) NOT NULL,
     codigo_del_cliente CHAR(4) NOT NULL,
     total_de_la_factura DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (sucursal, numero_de_factura),
@@ -22,11 +22,11 @@ CREATE TABLE Factura (
 
 CREATE TABLE Detalle_de_Factura (
     sucursal CHAR(2) NOT NULL,
-    numero_de_factura CHAR(3) NOT NULL,
+    numero_de_factura INTEGER NOT NULL, 
     codigo_de_articulo CHAR(4) NOT NULL,
     cantidad_del_articulo INTEGER NOT NULL,
-    precio_unitario_del_articulo DECIMAL(10,2) NOT NULL, 
-    subtotal_del_articulo DECIMAL(10,2) NOT NULL,        
+    precio_unitario_del_articulo DECIMAL(10,2) NOT NULL,
+    subtotal_del_articulo DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (sucursal, numero_de_factura, codigo_de_articulo),
     FOREIGN KEY (sucursal, numero_de_factura) REFERENCES Factura(sucursal, numero_de_factura),
     FOREIGN KEY (codigo_de_articulo) REFERENCES Articulo(codigo_del_articulo)
