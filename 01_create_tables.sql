aebae8f4f759f98026e693721b2afafa96d83c5b
Jpablo179804 <179804@upslp.edu.mx>
2025-10-03 10:15:57 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE Clientes (
 );
 
 CREATE TABLE Articulo (
-    codigo_del_articulo CHAR(4) PRIMARY KEY, 
+    codigo_del_articulo CHAR(4) PRIMARY KEY,
     nombre_del_articulo VARCHAR(100) NOT NULL,
     precio_unitario DECIMAL(10,2) NOT NULL
 );
@@ -13,7 +13,7 @@ CREATE TABLE Factura (
     sucursal CHAR(2) NOT NULL,
     numero_de_factura CHAR(3) NOT NULL,
     fecha_de_la_factura DATE NOT NULL,
-    forma_de_pago_factura CHAR(2) NOT NULL,
+    forma_de_pago_factura CHAR(8) NOT NULL,  
     codigo_del_cliente CHAR(4) NOT NULL,
     total_de_la_factura DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (sucursal, numero_de_factura),
@@ -23,10 +23,10 @@ CREATE TABLE Factura (
 CREATE TABLE Detalle_de_Factura (
     sucursal CHAR(2) NOT NULL,
     numero_de_factura CHAR(3) NOT NULL,
-    codigo_de_articulo CHAR(4) NOT NULL,  
+    codigo_de_articulo CHAR(4) NOT NULL,
     cantidad_del_articulo INTEGER NOT NULL,
-    precio_unitario DECIMAL(10,2) NOT NULL,
-    subtotal_articulo DECIMAL(10,2) NOT NULL,
+    precio_unitario_del_articulo DECIMAL(10,2) NOT NULL, 
+    subtotal_del_articulo DECIMAL(10,2) NOT NULL,        
     PRIMARY KEY (sucursal, numero_de_factura, codigo_de_articulo),
     FOREIGN KEY (sucursal, numero_de_factura) REFERENCES Factura(sucursal, numero_de_factura),
     FOREIGN KEY (codigo_de_articulo) REFERENCES Articulo(codigo_del_articulo)
