91c738d292043e2d71c41b5297b503a728275e94
Yovana-Palencia <174197@upslp.edu.mx>
2025-10-02 22:00:12 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,32 +1,31 @@
 CREATE TABLE clientes (
-    codigo_cliente VARCHAR(10) PRIMARY KEY,
-    nombre_cliente VARCHAR(100) NOT NULL
+    codigo_cliente SERIAL PRIMARY KEY,
+    nombre_cliente TEXT NOT NULL
 );
 
 CREATE TABLE articulos (
-    codigo_articulo VARCHAR(10) PRIMARY KEY,
-    nombre_articulo VARCHAR(100) NOT NULL,
-    precio_unitario_articulo NUMERIC(10,2) NOT NULL
+    codigo_articulo SERIAL PRIMARY KEY,
+    nombre_articulo TEXT NOT NULL,
+    precio_unitario_articulo DECIMAL(6,2) NOT NULL
 );
 
 CREATE TABLE facturas (
-    sucursal VARCHAR(10),
-    numero_factura VARCHAR(10),
+    sucursal INTEGER NOT NULL,
+    numero_factura INTEGER NOT NULL,
     fecha_factura DATE NOT NULL,
-    forma_pago_factura CHAR(1) NOT NULL,
-    codigo_cliente VARCHAR(10) NOT NULL,
-    total_factura NUMERIC(10,2) NOT NULL,
-    PRIMARY KEY (sucursal, numero_factura),
-    FOREIGN KEY (codigo_cliente) REFERENCES clientes(codigo_cliente)
+    forma_pago_factura TEXT NOT NULL,
+    codigo_cliente INTEGER NOT NULL REFERENCES clientes(codigo_cliente),
+    total_factura DECIMAL(6,2) NOT NULL,
+    PRIMARY KEY (sucursal, numero_factura)
 );
 
 CREATE TABLE detalle_factura (
-    sucursal VARCHAR(10),
-    numero_factura VARCHAR(10),
-    codigo_articulo VARCHAR(10),
+    sucursal INTEGER NOT NULL,
+    numero_factura INTEGER NOT NULL,
+    codigo_articulo INTEGER NOT NULL REFERENCES articulos(codigo_articulo),
     cantidad_articulo INTEGER NOT NULL,
-    subtotal_articulo NUMERIC(10,2) NOT NULL,
-    PRIMARY KEY (sucursal, numero_factura, codigo_articulo),
+    precioUnitario_articulo DECIMAL(6,2) NOT NULL,
+    subtotal_articulo DECIMAL(6,2) NOT NULL,
     FOREIGN KEY (sucursal, numero_factura) REFERENCES facturas(sucursal, numero_factura),
-    FOREIGN KEY (codigo_articulo) REFERENCES articulos(codigo_articulo)
+    PRIMARY KEY (sucursal, numero_factura, codigo_articulo)
 );
