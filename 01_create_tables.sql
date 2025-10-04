17081ebd6a0cd9cea8d3f0fe3d774b58af7cdaaf
Juan Pablo Csnv <175329@upslp.edu.mx>
2025-10-03 22:59:06 -0600

    create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,41 +1,37 @@
-CREATE TABLE forma_pago(
-  forma_de_pago_factura VARCHAR(5) PRIMARY KEY
+CREATE TABLE forma_de_pago (
+  forma_pago VARCHAR(10) PRIMARY KEY
 );
 
--- Tabla de clientes
-CREATE TABLE clientes(
-  codigo_del_cliente INTEGER PRIMARY KEY,
+CREATE TABLE clientes (
+  codigo_del_cliente VARCHAR(20) PRIMARY KEY,
   nombre_del_cliente VARCHAR(50) NOT NULL
 );
 
--- Tabla de artículos
-CREATE TABLE articulo(
-  codigo_del_articulo INTEGER PRIMARY KEY,
+CREATE TABLE articulo (
+  codigo_del_articulo VARCHAR(20) PRIMARY KEY,
   nombre_del_articulo VARCHAR(30) NOT NULL,
   precio_unitario FLOAT NOT NULL
 );
 
--- Tabla de facturas
-CREATE TABLE factura(
+CREATE TABLE factura (
   sucursal VARCHAR(30) NOT NULL,
   numero_de_factura INTEGER NOT NULL,
   fecha_de_la_factura DATE NOT NULL,
-  forma_de_pago_factura VARCHAR(5) NOT NULL,
-  codigo_del_cliente INTEGER NOT NULL,
-  total_de_la_factura FLOAT NOT NULL,
+  forma_de_pago_factura VARCHAR(10) NOT NULL,
+  codigo_del_cliente VARCHAR(20) NOT NULL,
+  total_de_la_factura FLOAT,
   PRIMARY KEY(sucursal, numero_de_factura),
   FOREIGN KEY(codigo_del_cliente) REFERENCES clientes(codigo_del_cliente),
-  FOREIGN KEY(forma_de_pago_factura) REFERENCES forma_pago(forma_de_pago_factura)
+  FOREIGN KEY(forma_de_pago_factura) REFERENCES forma_de_pago(forma_pago)
 );
 
--- Tabla de detalle de facturas
-CREATE TABLE detalle_de_factura(
+CREATE TABLE detalle_de_factura (
   sucursal VARCHAR(30) NOT NULL,
   numero_de_factura INTEGER NOT NULL,
-  codigo_de_articulo INTEGER NOT NULL,
+  codigo_de_articulo VARCHAR(20) NOT NULL,
   cantidad_del_articulo INTEGER NOT NULL,
   precio_unitario_del_articulo FLOAT NOT NULL,
-  subtotal_del_articulo FLOAT NOT NULL,
+  subtotal_del_articulo FLOAT GENERATED ALWAYS AS (cantidad_del_articulo * precio_unitario_del_articulo) STORED,
   PRIMARY KEY(sucursal, numero_de_factura, codigo_de_articulo),
   FOREIGN KEY(codigo_de_articulo) REFERENCES articulo(codigo_del_articulo),
   FOREIGN KEY(sucursal, numero_de_factura) REFERENCES factura(sucursal, numero_de_factura)
