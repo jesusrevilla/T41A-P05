cc11c3e4ea59f8a38a25886fe833134d1b0b99c9
ItsMusa17 <160455957+ItsMusa17@users.noreply.github.com>
2025-10-06 08:27:01 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,33 @@
+CREATE TABLE cliente(
+  codigo_cliente INTEGER PRIMARY KEY,
+  nombre_cliente TEXT NOT NULL 
+); 
 
+CREATE TABLE articulo(
+  codigo_articulo INTEGER PRIMARY KEY,
+  nombre_articulo TEXT NOT NULL,
+  precio_unitario DECIMAL(6,2) NOT NULL
+); 
+
+CREATE TABLE facturas (
+  sucursal INTEGER NOT NULL,
+  num_factura INTEGER NOT NULL,
+  fecha_factura DATE NOT NULL,
+  forma_pago TEXT NOT NULL,
+  codigo_cliente INTEGER NOT NULL, 
+  total_factura DECIMAL(6,2) NOT NULL,
+  PRIMARY KEY(sucursal,num_factura),
+  FOREIGN KEY (codigo_cliente) REFERENCES cliente(codigo_cliente)
+);
+
+CREATE TABLE detalle_factura(
+  sucursal INTEGER NOT NULL,
+  num_factura INTEGER NOT NULL,
+  codigo_articulo INTEGER NOT NULL,
+  cantidad_articulo INTEGER NOT NULL,
+  precioUnitario_articulo DECIMAL(6,2) NOT NULL,
+  subtotal_articulo DECIMAL(6,2) NOT NULL,
+  FOREIGN KEY (sucursal,num_factura) REFERENCES facturas(sucursal,num_factura),
+  FOREIGN KEY (codigo_articulo) REFERENCES articulo(codigo_articulo),
+  PRIMARY KEY (sucursal,num_factura,codigo_articulo)
+);
