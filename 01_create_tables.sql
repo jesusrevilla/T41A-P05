823b735c3d4db507055c1d360a63323d5610169f
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-10-06 00:48:20 -0600

    feature/182483

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,4 +1,3 @@
---Tablas en 3FN
 -- Tabla CLIENTES
 CREATE TABLE clientes (
     codigo_del_cliente VARCHAR(10) PRIMARY KEY,
@@ -34,15 +33,8 @@ CREATE TABLE detalle_de_factura (
     precio_unitario_del_articulo NUMERIC(10,2) NOT NULL,
     subtotal_del_articulo NUMERIC(10,2) NOT NULL,
     PRIMARY KEY (sucursal, numero_de_factura, codigo_de_articulo),
-
-    -- Claves foráneas que los tests esperan:
     FOREIGN KEY (sucursal, numero_de_factura)
         REFERENCES factura (sucursal, numero_de_factura),
     FOREIGN KEY (codigo_de_articulo)
-        REFERENCES articulo (codigo_del_articulo),
-    FOREIGN KEY (sucursal)
-        REFERENCES factura (sucursal),
-    FOREIGN KEY (numero_de_factura)
-        REFERENCES factura (numero_de_factura)
+        REFERENCES articulo (codigo_del_articulo)
 );
-
