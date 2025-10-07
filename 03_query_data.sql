e387d60d431247e524fff2f808aacbe14eda8324
EmmanuelC-137 <160262587+EmmanuelC-137@users.noreply.github.com>
2025-10-06 19:16:16 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,9 +1,11 @@
 SELECT 
-SELECT
-    f.sucursal,
-    f.numero_de_factura,
-    f.fecha_de_la_factura,
-@@ -15,4 +15,3 @@ JOIN clientes c ON f.codigo_del_cliente = c.codigo_del_cliente
-JOIN detalle_de_factura df ON f.sucursal = df.sucursal AND f.numero_de_factura = df.numero_de_factura
-JOIN articulo a ON df.codigo_de_articulo = a.codigo_del_articulo
-WHERE f.sucursal = 'S1' AND f.numero_de_factura = 1;
+    c.nom_cliente,
+    a.nom_articulo,
+    df.cantidad
+FROM detalle_factura df
+JOIN factura f 
+    ON df.num_fac = f.num_fac AND df.sucursal = f.sucursal
+JOIN cliente c 
+    ON f.cod_cliente = c.cod_cliente
+JOIN articulo a 
+    ON df.cod_articulo = a.cod_articulo;
