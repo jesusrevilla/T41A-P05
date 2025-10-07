660ef5d6178f1113b8d81a85d24b8f923ec4e5b5
EmmanuelC-137 <160262587+EmmanuelC-137@users.noreply.github.com>
2025-10-06 19:41:58 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,11 +1,35 @@
-SELECT 
-    c.nom_cliente,
-    a.nom_articulo,
-    df.cantidad
-FROM detalle_factura df
-JOIN factura f 
-    ON df.num_fac = f.num_fac AND df.sucursal = f.sucursal
-JOIN cliente c 
-    ON f.cod_cliente = c.cod_cliente
-JOIN articulo a 
-    ON df.cod_articulo = a.cod_articulo;
+SELECT column_name
+FROM information_schema.columns
+WHERE table_name = 'factura'
+ORDER BY ordinal_position;
+
+-- PKs y FKs para 'factura'
+SELECT tc.constraint_type, kcu.column_name, tc.constraint_name
+FROM information_schema.table_constraints tc
+JOIN information_schema.key_column_usage kcu
+  ON tc.constraint_name = kcu.constraint_name
+WHERE tc.table_name = 'factura';
+
+-- Ver columnas de 'detalle_de_factura'
+SELECT column_name
+FROM information_schema.columns
+WHERE table_name = 'detalle_de_factura'
+ORDER BY ordinal_position;
+
+-- PKs y FKs para 'detalle_de_factura'
+SELECT tc.constraint_type, kcu.column_name, tc.constraint_name
+FROM information_schema.table_constraints tc
+JOIN information_schema.key_column_usage kcu
+  ON tc.constraint_name = kcu.constraint_name
+WHERE tc.table_name = 'detalle_de_factura';
+
+-- Ver datos insertados
+SELECT * FROM clientes;
+SELECT * FROM articulo;
+SELECT * FROM factura;
+SELECT * FROM detalle_de_factura;
+
+-- Consulta que junta factura y su detalle
+SELECT f.sucursal, f.numero_de_factura, f.codigo_del_cliente, d.codigo_de_articulo, d.cantidad_del_articulo, d.precio_unitario_del_articulo, d.subtotal_del_articulo
+FROM factura f
+JOIN detalle_de_factura d ON f.sucursal = d.sucursal AND f.numero_de_factura = d.numero_de_factura;
