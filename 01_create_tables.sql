8207941b25291530a14c26ba64a113bdd6211cd7
Yovana-Palencia <174197@upslp.edu.mx>
2025-10-07 18:24:06 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,34 @@
+--Yovana Isabel Palencia Sánchez 174197
 
+CREATE TABLE clientes (
+    codigo_cliente VARCHAR(10) PRIMARY KEY,
+    nombre_cliente VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE articulos (
+    codigo_articulo VARCHAR(10) PRIMARY KEY,
+    nombre_articulo VARCHAR(100) NOT NULL,
+    precio_unitario_articulo NUMERIC(10,2) NOT NULL
+);
+
+CREATE TABLE facturas (
+    sucursal VARCHAR(10),
+    numero_factura VARCHAR(10),
+    fecha_factura DATE NOT NULL,
+    forma_pago_factura CHAR(1) NOT NULL,
+    codigo_cliente VARCHAR(10) NOT NULL,
+    total_factura NUMERIC(10,2) NOT NULL,
+    PRIMARY KEY (sucursal, numero_factura),
+    FOREIGN KEY (codigo_cliente) REFERENCES clientes(codigo_cliente)
+);
+
+CREATE TABLE detalle_factura (
+    sucursal VARCHAR(10),
+    numero_factura VARCHAR(10),
+    codigo_articulo VARCHAR(10),
+    cantidad_articulo INTEGER NOT NULL,
+    subtotal_articulo NUMERIC(10,2) NOT NULL,
+    PRIMARY KEY (sucursal, numero_factura, codigo_articulo),
+    FOREIGN KEY (sucursal, numero_factura) REFERENCES facturas(sucursal, numero_factura),
+    FOREIGN KEY (codigo_articulo) REFERENCES articulos(codigo_articulo)
+);
