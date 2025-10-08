9cda78f80293eda9141f6b5dcb92e45c28ba8b4f
Yovana-Palencia <174197@upslp.edu.mx>
2025-10-07 18:25:20 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,1 +1,12 @@
-
+SELECT 
+    f.sucursal,
+    f.numero_factura,
+    f.fecha_factura,
+    c.nombre_cliente,
+    a.nombre_articulo,
+    df.cantidad_articulo,
+    df.subtotal_articulo
+FROM facturas f
+JOIN clientes c ON f.codigo_cliente = c.codigo_cliente
+JOIN detalle_factura df ON f.sucursal = df.sucursal AND f.numero_factura = df.numero_factura
+JOIN articulos a ON df.codigo_articulo = a.codigo_articulo;
